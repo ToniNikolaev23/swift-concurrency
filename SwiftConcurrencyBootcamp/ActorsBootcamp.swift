c555f83b95edbf959cd207ca5580cc2533eb277c
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-02 21:58:35 +0200

    Actors

---
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -0,0 +1,149 @@
+//
+//  ActorsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Toni Stoyanov on 2.01.25.
+//
+
+import SwiftUI
+
+class MyDataManager {
+    static let instance = MyDataManager()
+    
+    private init() {}
+    
+    var data: [String] = []
+    private let queue = DispatchQueue(label: "com.MyApp.MyDataManager")
+    
+    func getRandomData(completionHandler: @escaping (_ title: String?) -> Void) {
+        queue.async {
+            self.data.append(UUID().uuidString)
+            print(Thread.current)
+            completionHandler(self.data.randomElement())
+        }
+       
+    }
+    
+}
+
+actor MyActorDataManager {
+    static let instance = MyActorDataManager()
+    
+    private init() {}
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return self.data.randomElement()
+       
+    }
+    
+    nonisolated func getSavedData() -> String {
+        return "New data"
+    }
+    
+}
+
+struct HomeView: View {
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onAppear {
+            let newString = manager.getSavedData()
+//            Task {
+//                let newString = manager.getSavedData()
+//            }
+        }
+        .onReceive(timer, perform: { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+//            DispatchQueue.global(qos: .background).async {
+//                
+//                manager.getRandomData { title in
+//                    if let data = title{
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                       
+//                    }
+//                }
+//                
+//               
+//            }
+           
+        })
+    }
+}
+
+struct BrowseView: View {
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+                                
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer, perform: { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+//            DispatchQueue.global(qos: .background).async {
+//                
+//                manager.getRandomData { title in
+//                    if let data = title{
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                       
+//                    }
+//                }
+//                
+//               
+//            }
+           
+        })
+    }
+}
+
+struct ActorsBootcamp: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            BrowseView()
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+#Preview {
+    ActorsBootcamp()
+}
